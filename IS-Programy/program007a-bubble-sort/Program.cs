3bb14ef01ce79eaddeda5efd1e5087a613f7fc06
Anežka Husáková <anezka.husakova@tul.cz>
2025-11-20 13:42:50 +0100

    #10 Řazení pomocí Bubble sortu

--- IS-Programy/program007a-bubble-sort/Program.cs
+++ IS-Programy/program007a-bubble-sort/Program.cs
@@ -1,4 +1,6 @@
-﻿string again = "a";
+﻿using System.Diagnostics;
+
+string again = "a";
 while (again == "a")
 {
     Console.Clear();
@@ -50,19 +52,71 @@ while (again == "a")
     //příprava pro využití třídy Random
     Random myRandNumb = new Random();
     //Random myRandNumb = new Random(15); stejný vstup --> stejné výsledky
-    
+
 
     Console.WriteLine();
     Console.WriteLine("Náhodná čísla: ");
     for (int i = 0; i < n; i++)
-    
+
     {
-        myRandNumbs[i] = myRandNumb.Next(lowerBound, upperBound+1);
-        Console.Write("{0};", myRandNumbs [i]);
+        myRandNumbs[i] = myRandNumb.Next(lowerBound, upperBound + 1);
+        Console.Write("{0};", myRandNumbs[i]);
 
 
     }
-     
+
+    Stopwatch myStopwatch = new Stopwatch();
+
+    int compare = 0; //počet porovnávání, ke kterým došlo
+    int change = 0;  //počet výměn
+
+
+    myStopwatch.Start();
+
+    for (int i = 0; i < n - 1; i++) //n - 1 - abychom nepřešvihli pole. i < n - aby to projelo všechny čísla
+    {
+        //tento cyklus musí zajistit porovnávání dvou sousedních hodnot
+        // musí dále zajistit aby se zmenšoval počet porovnávaných hodnot
+        for (int j = 0; j < n - i - 1; j++)
+        {
+            if (myRandNumbs[j] > myRandNumbs[j + 1])
+            {
+                int tmp = myRandNumbs[j + 1]; //to co je vpravo se objeví na dalším řádku vlevo
+                myRandNumbs[j + 1] = myRandNumbs[j];
+                myRandNumbs[j] = tmp;
+                compare++;
+            }
+            change++;
+        }
+
+
+    }
+    myStopwatch.Stop();
+
+
+    Console.WriteLine("");
+    Console.WriteLine("=====================================");
+    Console.WriteLine("Seřazená pole: ");
+    for (int i = 0; i < n; i++)
+    {
+        Console.Write("{0}, ", myRandNumbs[i]);
+    }
+    Console.WriteLine("");
+    Console.WriteLine("\nPočet porovnávání: {0}", compare);
+    Console.WriteLine("Počet výměn: {0}", change);
+
+    Console.WriteLine("");
+    Console.WriteLine("Čas seřazení pomocí bubble sort: {0}", myStopwatch.Elapsed);
+    Console.WriteLine("");
+        
+   
+
+    // Console.WriteLine("");
+    // Console.WriteLine("");
+    // Console.WriteLine("");
+    // Console.WriteLine("");
+    // Console.WriteLine("");
+
 
     Console.WriteLine();
     Console.WriteLine("Pro opakování programu stiskněte klávesu a");
--- IS-Programy/program007a-bubble-sort/obj/Debug/net9.0/program007a-bubble-sort.AssemblyInfo.cs
+++ IS-Programy/program007a-bubble-sort/obj/Debug/net9.0/program007a-bubble-sort.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("program007a-bubble-sort")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+24b7e0f356895e85b112d3f99f4c42a90c26ccb2")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d5e1363a61569af0f7e9398e1c34dd91b2372a60")]
 [assembly: System.Reflection.AssemblyProductAttribute("program007a-bubble-sort")]
 [assembly: System.Reflection.AssemblyTitleAttribute("program007a-bubble-sort")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
